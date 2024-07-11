8b6015cc0cad64f84223f97311c5a324ecd3bb9b
unknown <monikapatidar1009@gmail.com>
2024-07-11 15:15:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+function App() {
+    return(
+        <>
+            <Home/>
+            <Contact/>
+        </>
+    );
+}
+export default App;
---
+++ src/pages/Contact.js
@@ -0,0 +1,8 @@
+function Contact() {
+    return(
+    <>
+    <h1>wlc to Contact page</h1>
+    </>
+    );
+    }
+    export default Contact;
